4a2b05d591bc435691f1f24e586c8283ac037efb
Your Name <you@example.com>
2021-04-20 19:53:36 -0300

    Initial commit de spring  boot

---
+++ src/main/java/utn/gallino/mspagos/MsPagosApplication.java
@@ -0,0 +1,13 @@
+package utn.gallino.mspagos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPagosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPagosApplication.class, args);
+	}
+
+}
